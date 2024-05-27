24ce158852e7f74347c515030f92454f1b744f79
Rifna <102509964+RifnaC@users.noreply.github.com>
2024-05-27 12:31:39 +0530

    update the the package.json

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb+srv://admin:admin123@cluster0.ztlosb7.mongodb.net/SnapStash?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://admin:admin123@cluster0.ztlosb7.mongodb.net/SnapStash?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use("/api/users", userRoutes);
 
